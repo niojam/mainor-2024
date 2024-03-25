2f7f86ee384c366cd4a98fc224ca910f848b3b48
nikitaojamae <nikita205>
2024-03-25 18:56:38 +0200

    Classroom session 2

---
+++ src/main/java/ee/mainor/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package ee.mainor.demo.repository;
+
+import ee.mainor.demo.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    List<User> findAll();
+
+}
--- src/main/java/ee/mainor/demo/service/ProductService.java
+++ src/main/java/ee/mainor/demo/service/ProductService.java
@@ -3,11 +3,21 @@ package ee.mainor.demo.service;
 import ee.mainor.demo.dto.CreateProductRequest;
 import ee.mainor.demo.dto.ProductDto;
 import ee.mainor.demo.mapper.ProductMapper;
+import ee.mainor.demo.model.User;
+import ee.mainor.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
 
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final UserRepository userRepository;
+    private final Random random = new Random();
 
     public ProductDto create(CreateProductRequest createProductRequest) {
         // saving goes here
